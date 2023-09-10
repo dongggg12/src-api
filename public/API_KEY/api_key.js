c11e64334a6832c4066f622ca76fffcc129b82cc
Minh Dong <136040904+MDong-06@users.noreply.github.com>
2023-09-10 11:55:28 +0700

    Update api_key.js

--- public/API_KEY/api_key.js
+++ public/API_KEY/api_key.js
@@ -7,25 +7,25 @@ exports.index = async(req, res, next) => {
         fs.writeFileSync(path_D, '[]', 'utf-8');
     }
     const data_apikey = require('./data/data_apikey.json');
-    if (data_apikey.find(i => i.username == req.query.username)) {
+    if (data_apikey.find(i => i.name == req.query.name)) {
         return res.json({
-            error: 'Bạn đã có APIKEY trên hệ thống'
+            error: 'Bạn đã có key trên hệ thống'
         });
     }
-    if (req.query.keytype == 'register') {
-        let username = req.query.username;
-        if (!username) return res.json({
-            error: 'Thiếu dữ liệu để thực hiện yêu cầu cho bạn'
+    if (req.query.type == 'register') {
+        let name = req.query.name;
+        if (!name) return res.json({
+            error: 'Thiếu dữ liệu để thực hiện yêu cầu cho bạn.'
         });
         else {
-            if (req.query.apikey == 'ThanhAliVip_020599') {
-                var keytype = 'premium';
-                var apikey = 'ThanhAliVip_';
+            if (req.query.apikey == 'mzkilVip_020599') {
+                var type = 'premium';
+                var apikey = 'MDongVip_';
                 var request = 'infinite';
             } else {
-                var keytype = 'free';
-                var request = 500;
-                var apikey = 'ThanhAliFree_';
+                var type = 'free';
+                var request = 200;
+                var apikey = 'MDongFree_';
             }
             const data = require('./data/data_apikey.json');
             var random = '1234567890';
@@ -33,13 +33,13 @@ exports.index = async(req, res, next) => {
             for (var i = 0; i < number; i++) {
                 apikey += random.charAt(Math.floor(Math.random() * random.length));
             }
-            data.push({ apikey, username, request, keytype });
+            data.push({ apikey, name, request, type });
             fs.writeFileSync(path_D, JSON.stringify(data, null, 2), 'utf-8');
             res.json({
                 success: 200,
                 apikey,
-                keytype,
-                message: 'Tạo APIKEY thành công'
+                type,
+                message: 'Tạo apikey premium thành công!'
             })
         }
     } else if (req.query.type == 'checker') {
@@ -47,7 +47,7 @@ exports.index = async(req, res, next) => {
         const data = require('./data/data_apikey.json');
         if (!data.find(i => i.apikey == apikey)) {
             return res.json({
-                error: 'APIKEY không tồn tại'
+                error: 'APIKEY không tồn tại!'
             })
         } else {
             var APIKEY = data.find(i => i.apikey == apikey);
